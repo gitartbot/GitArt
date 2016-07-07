aeee2757c1b5f8b635c7dbae66f2ff70f932c6f7
Nicolas <nicolas@shareactor.io>
2016-07-07 15:08:58 +0200

    gitart

--- gitart.py
+++ gitart.py
@@ -1,10 +1,11 @@
 import time
 import os
-import array
+from array import array
 
 colors = array('i', [0, 1, 4, 6, 10])
 day = int(time.strftime("%j"))
 
 for i in range(0, colors[day % len(colors)]):
     os.system('echo "." >> README.md')
-    
+
+os.system('git add -A && git commit -m "gitart"')
