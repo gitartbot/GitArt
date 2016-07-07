32eded70db08f81d5a805c7bcfb3ae44a2b19997
Nicolas <nicolas@shareactor.io>
2016-07-07 15:18:10 +0200

    gitart

--- gitart.py
+++ gitart.py
@@ -7,5 +7,6 @@ day = int(time.strftime("%j"))
 
 for i in range(0, colors[day % len(colors)]):
     os.system('echo "." >> README.md')
+    os.system('git add -A && git commit -m "gitart"')
 
-os.system('git add -A && git commit -m "gitart"')
+os.system('git push')
